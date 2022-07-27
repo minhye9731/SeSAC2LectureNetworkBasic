a1719cedaf4728f669006170cf9ce72e3d5952d4
minhye9731 <jenney62639731@gmail.com>
2022-07-27 11:42:18 +0900

    Initial Commit

---
+++ SeSAC2LectureNetworkBasic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC2LectureNetworkBasic
+//
+//  Created by 강민혜 on 7/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
